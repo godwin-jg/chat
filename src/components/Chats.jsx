32b46e7878fde6eaa9f18ec227e48a0e2b231de2
Godwin <jgnsecrets@gmail.com>
2023-01-30 23:20:13 +0530

    setup gh pages

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -29,7 +29,7 @@ const Chats = () => {
   };
 
   return (
-    <div className="chats">
+    <div className="messages">
       {Object.entries(chats || {})?.sort((a, b) => b[1].date - a[1].date).map((chat) => (
         <div
           className="userChat"
